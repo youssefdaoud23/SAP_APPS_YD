26733d957510935b959c565f8c6a553d2b56170f
Youssef Daoud <31803158+youssefdaoud23@users.noreply.github.com>
2026-09-15 17:17:09 +0200

    Update PWA cache for secure connector UI

--- sw.js
+++ sw.js
@@ -1,12 +1,16 @@
-const CACHE = 'invarture-app-studio-v2';
-const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest'];
-self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
-self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
+const CACHE = 'invarture-app-studio-v3';
+const ASSETS = ['./', './index.html', './styles.css', './app.js', './connection-center.js', './manifest.webmanifest'];
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
+self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
+  const url = new URL(event.request.url);
+  if (url.pathname.startsWith('/api/')) return;
   event.respondWith(fetch(event.request).then(response => {
-    const clone = response.clone();
-    caches.open(CACHE).then(cache => cache.put(event.request, clone)).catch(() => {});
+    if (response.ok) {
+      const clone = response.clone();
+      caches.open(CACHE).then(cache => cache.put(event.request, clone)).catch(() => {});
+    }
     return response;
   }).catch(() => caches.match(event.request).then(r => r || caches.match('./index.html'))));
 });
