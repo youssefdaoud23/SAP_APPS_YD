323e10c9ebf28202782f3979ba52cc21bb36df93
Youssef Daoud <31803158+youssefdaoud23@users.noreply.github.com>
2026-09-15 17:19:28 +0200

    Cache live SAP data adapter in PWA shell

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'invarture-app-studio-v3';
-const ASSETS = ['./', './index.html', './styles.css', './app.js', './connection-center.js', './manifest.webmanifest'];
+const CACHE = 'invarture-app-studio-v4';
+const ASSETS = ['./', './index.html', './styles.css', './app.js', './connection-center.js', './live-data.js', './manifest.webmanifest'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => {
